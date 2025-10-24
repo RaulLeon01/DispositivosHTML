ef2e9e83e560a3a63eee0d917938e4a3fd68d86e
RaulLeon01 <20243rd013@utez.edu.mx>
2025-10-24 15:51:18 -0600

    Estructura del proyecto
    Agregadas las carpetas y el archivo HTML

--- src/app.py
+++ src/app.py
@@ -1,65 +1,18 @@
-from flask import Flask, jsonify, render_template_string, request, redirect, url_for
+# INSTALACIÓN DE FLASK:
+# 1. cd C:/Users/TuUsuario/Documents/Flask_
+# 2. python -m venv venv
+# 3. venv\Scripts\activate
+# 4. pip install flask
+# 5. pip show flask
+# 6. python app.py
 
-app = Flask(__name__)
+from flask import Flask, send_from_directory
 
-# Lista para almacenar los dispositivos
-dispositivos_registrados = []
+app = Flask(__name__, static_folder='src', static_url_path='')
 
-@app.route('/formulario')
-def formulario():
-    return render_template_string("""
-    <html>
-    <head><title>Agregar Dispositivo</title></head>
-    <body>
-        <h1>Nuevo Dispositivo</h1>
-        /agregar
-            SID: <input type="text" name="sid"><br>
-            Nombre: <input type="text" name="nombre"><br>
-            IP: <input type="text" name="ip"><br>
-            Protocolos (separados por coma): <input type="text" name="protocolos"><br>
-            Observaciones: <input type="text" name="observaciones"><br>
-            <input type="submit" value="Agregar">
-        </form>
-    </body>
-    </html>
-    """)
-
-@app.route('/agregar', methods=['POST'])
-def agregar_dispositivo():
-    data = {
-        "sid": request.form['sid'],
-        "nombre": request.form['nombre'],
-        "ip": request.form['ip'],
-        "protocolos": [p.strip() for p in request.form['protocolos'].split(',')],
-        "observaciones": request.form['observaciones']
-    }
-    dispositivos_registrados.append(data)
-    return redirect(url_for('mostrar_dispositivos'))
-
-@app.route('/dispositivos')
-def mostrar_dispositivos():
-    html = ""
-    for d in dispositivos_registrados:
-        html += f"""
-        <div style="border:1px solid #ccc; padding:10px; margin:10px;">
-            <strong>{d['sid']}</strong><br>
-            <b>Nombre:</b> {d['nombre']}<br>
-            <b>IP:</b> {d['ip']}<br>
-            <b>Protocolos:</b> {', '.join(d['protocolos'])}<br>
-            <b>Observaciones:</b> {d['observaciones']}
-        </div>
-        """
-
-    return render_template_string(f"""
-    <html>
-    <head><title>Dispositivos de Red</title></head>
-    <body>
-        <h1>Lista de Dispositivos</h1>
-        /formularioAgregar nuevo dispositivo</a>
-        {html}
-    </body>
-    </html>
-    """)
+@app.route('/')
+def serve_index():
+    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
